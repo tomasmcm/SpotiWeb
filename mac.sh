e8891bdd0816cf0af6cd065981e0e955cf3f6089
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2016-04-16 17:11:20 +0100

    change from zip to tar
    .zip extension was create a huge .app file on extraction

--- mac.sh
+++ mac.sh
@@ -6,6 +6,6 @@ rm -rf ./dist/SpotiWeb-darwin-x64.zip
 
 cd releases
 echo '#### Compressing linux ia32 ####'
-zip -r -X ../dist/SpotiWeb-darwin-x64.zip ./SpotiWeb-darwin-x64/SpotiWeb.app
+tar -zcvf ../dist/SpotiWeb-darwin-x64.tar.gz ./SpotiWeb-darwin-x64/SpotiWeb.app
 
 echo '#### Done Mac version ####'

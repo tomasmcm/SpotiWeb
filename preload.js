6961f2c16cd0956664306f49ee58840d5e53a6d5
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2016-04-17 13:11:42 +0100

    reload page if `can't connect to spotify` error

--- preload.js
+++ preload.js
@@ -1,4 +1,5 @@
-const ipcRenderer = require('electron').ipcRenderer;
+var ipcRenderer = require('electron').ipcRenderer;
+var remote = require('remote');
 
 window.notify = function(music, author) {
   var imageURL = document.getElementById('app-player').contentDocument.getElementById('cover-art').querySelector(".sp-image-img").style.backgroundImage.slice(5, -2);
@@ -21,4 +22,11 @@ window.onload = function(){
   document.body.appendChild(draggableArea);
   document.getElementsByTagName("html")[0].style.cssText += "overflow: hidden;height: 100%;";
   document.getElementById('now-playing-widgets').style.cssText += "display: none;";
+
+  setInterval(function(){
+    if(document.getElementById('modal-notification-area').style.display !== 'none'){
+      remote.getCurrentWindow().reload();
+    }
+  }, 1000);
+
 }

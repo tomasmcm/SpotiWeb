da020d56d277f6977344d70b35bd2a7cc4391c40
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2016-04-22 11:03:48 +0100

    disable 64bits export

--- win.sh
+++ win.sh
@@ -3,12 +3,12 @@
 echo '#### Removing old dists ####'
 mkdir -p ./dist
 rm -rf ./dist/SpotiWeb-win32-ia32.zip
-rm -rf ./dist/SpotiWeb-win32-x64.zip
+# rm -rf ./dist/SpotiWeb-win32-x64.zip
 
 cd releases
 echo '#### Compressing win ia32 ####'
 zip -r -X ../dist/SpotiWeb-win32-ia32.zip ./SpotiWeb-win32-ia32
-echo '#### Compressing win x64 ####'
-zip -r -X ../dist/SpotiWeb-win32-x64.zip ./SpotiWeb-win32-x64
+# echo '#### Compressing win x64 ####'
+# zip -r -X ../dist/SpotiWeb-win32-x64.zip ./SpotiWeb-win32-x64
 
 echo '#### Done Windows versions ####'

3412565e6d60c63acd0cddc32d42873767c50ae7
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2017-04-02 16:23:08 +0100

    new className update

--- preload.js
+++ preload.js
@@ -8,16 +8,17 @@ window.setCurrentImage = function(val){
 };
 
 window.notify = function() {
+  clearAdDivs();
   var imageURL;
   if(currentImage == null) {
-    imageURL = document.getElementById('app-player').contentDocument.getElementById('cover-art').querySelector(".sp-image-img").style.backgroundImage.slice(5, -2);
+    imageURL = document.querySelector(".now-playing-bar .cover-art-image").style.backgroundImage.slice(5, -2);
   } else {
     imageURL = currentImage;
     currentImage = null;
   }
 
-  var title = document.getElementById('app-player').contentDocument.getElementById('track-name').querySelector("a").text;
-  var author = document.getElementById('app-player').contentDocument.getElementById('track-artist').querySelector("a").text;
+  var title = document.querySelector(".now-playing-bar .track-info__name").querySelector("a").text;
+  var author = document.querySelector(".now-playing-bar .track-info__artists").querySelector("a").text;
 
   if(process.platform === 'linux') {
     try { notification.close(); } catch (e) { }
@@ -43,34 +44,39 @@ window.notify = function() {
 }
 
 window.playerKey = function(command){
-  document.getElementById('app-player').contentDocument.getElementById(command).click();
-}
-
-window.lyricsLoaded = function(){
-  document.getElementById('app-player').contentDocument.querySelector("#lyrics_loading__js").style.display = "none";
-  if(lyricsTimeout != null){
-    clearTimeout(lyricsTimeout);
+  document.querySelector(".player-controls .control-button[Title="+command+"]").click();
+  if(command == "Play") {
+    notify();
   }
 }
 
+window.lyricsLoaded = function(){
+  if(lyricsTimeout != null){
+    clearTimeout(lyricsTimeout);
+  }
+  document.querySelector("#lyrics__js").classList.remove("spoticon-loading-16");
+}
+
 window.showLyrics = function(){
-  remote.getGlobal('currentSong').title = document.getElementById('app-player').contentDocument.getElementById('track-name').querySelector("a").text;
-  remote.getGlobal('currentSong').author = document.getElementById('app-player').contentDocument.getElementById('track-artist').querySelector("a").text;
-  document.getElementById('app-player').contentDocument.querySelector("#lyrics_loading__js").style.display = "inline-block";
+  document.querySelector("#lyrics__js").classList.add("spoticon-loading-16");
+  remote.getGlobal('currentSong').title = document.querySelector(".now-playing-bar .track-info__name").querySelector("a").text;
+  remote.getGlobal('currentSong').author = document.querySelector(".now-playing-bar .track-info__artists").querySelector("a").text;
   ipcRenderer.send('showLyrics');
 }
 
 window.updateLyricsButton = function(){
   try {
-    var title = document.getElementById('app-player').contentDocument.getElementById('track-name').querySelector("a").text;
-    var lyricsButton = document.getElementById('app-player').contentDocument.querySelector("#lyrics__js");
+    var title = document.querySelector(".now-playing-bar .track-info__name").querySelector("a").text;
+    var lyricsButton = document.querySelector("#lyrics__js");
     if(title == " "){
       if(!lyricsButton.classList.contains("disabled")){
         lyricsButton.classList.add("disabled");
+        lyricsButton.classList.add("btn");
       }
     } else {
       if(lyricsButton.classList.contains("disabled")){
         lyricsButton.classList.remove("disabled");
+        lyricsButton.classList.remove("btn");
       }
     }
   } catch (e) {
@@ -82,12 +88,22 @@ window.updateLyricsButton = function(){
 
 window.appendLyricsButton = function(){
   var lyricsButton = document.createElement('div');
-  lyricsButton.innerHTML = '<div class="btn btn-small disabled" style="margin-top:10px;cursor:pointer;" id="lyrics__js"><span class="spoticon-messages-16"></span> Lyrics <div id="lyrics_loading__js" style="background: url(' + remote.getGlobal('loadingGif') + ');width:14px;height:14px;background-size:contain;display:none;vertical-align:text-bottom;"></div></div>';
+  lyricsButton.innerHTML = '<button class="control-button spoticon-mic-16" title="Lyrics" style="cursor:pointer;" id="lyrics__js"><div id="lyrics_loading__js" style="background: url(' + remote.getGlobal('loadingGif') + ');width:14px;height:14px;background-size:contain;display:none;vertical-align:text-bottom;"></div></button>';
+
+  var style = document.createElement("style");
+  document.head.appendChild(style);
+  var sheet = style.sheet;
+  sheet.addRule('.spoticon-mic-16::before','content: "\\F32A"');
+  sheet.addRule('.spoticon-mic-16::before','font-size: 16px');
+
+  sheet.addRule('.spoticon-loading-16::before','content: "\\F3B6"');
 
   try {
-    document.getElementById('app-player').contentDocument.querySelector(".extra").appendChild(lyricsButton.childNodes[0]);
+    document.querySelector(".extra-controls").insertBefore(lyricsButton.childNodes[0], document.querySelector(".connect-device-picker"));
+
     setTimeout(function(){
-      document.getElementById('app-player').contentDocument.querySelector("#lyrics__js").addEventListener("click", function(){
+
+      document.querySelector("#lyrics__js").addEventListener("click", function(){
         window.showLyrics();
         lyricsTimeout = setTimeout(function(){
           alert("No Lyrics Found...");
@@ -113,19 +129,23 @@ window.onload = function(){
     document.body.appendChild(draggableArea);
   }
 
-  //document.getElementsByTagName("html")[0].style.cssText += "overflow: hidden;height: 100%;";
-  document.getElementById('now-playing-widgets').style.cssText += "display: none;";
-
   setTimeout(function(){
-    updateLyricsButton();
+    appendLyricsButton();
+
+    clearAdDivs();
+    if (process.platform === 'darwin') document.querySelector('.navBar').style.paddingTop = "39px";
   }, 500);
 
-  if (process.platform === 'darwin') document.querySelector('.main-menu.narrow-menu').style.paddingTop = "15px";
 
-  setInterval(function(){
-    if(document.getElementById('modal-notification-area').style.display === 'block'){
-      remote.getCurrentWindow().reload();
-    }
-  }, 2000);
+  // setInterval(function(){
+  //   if(document.getElementById('modal-notification-area').style.display === 'block'){
+  //     remote.getCurrentWindow().reload();
+  //   }
+  // }, 2000);
 
+}
+
+window.clearAdDivs = function(){
+  document.querySelector(".ads-container").style.display="none";
+  document.querySelector(".navlist-item.download-item").style.display="none";
 }

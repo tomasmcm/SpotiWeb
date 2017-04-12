64f3ed1825a513c53a94fc42cb870b36b47edc5f
tomasmcm <tomasmanuelcorreiamarques@gmail.com>
2017-04-12 20:49:34 +0100

    change folder name

--- mac.sh
+++ mac.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo '#### Removing old dists ####'
-mkdir -p ./dist
-rm -rf ./dist/SpotiWeb-darwin-x64.zip
+mkdir -p ./distributions
+rm -rf ./distributions/SpotiWeb-darwin-x64.zip
 
 cd releases
-echo '#### Compressing linux ia32 ####'
-tar -zcvf ../dist/SpotiWeb-darwin-x64.tar.gz ./SpotiWeb-darwin-x64/SpotiWeb.app
+echo '#### Compressing mac ####'
+tar -zcvf ../distributions/SpotiWeb-darwin-x64.tar.gz ./SpotiWeb-darwin-x64/SpotiWeb.app
 
 echo '#### Done Mac version ####'
--- win.sh
+++ win.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo '#### Removing old dists ####'
-mkdir -p ./dist
-rm -rf ./dist/SpotiWeb-win32-ia32.zip
-# rm -rf ./dist/SpotiWeb-win32-x64.zip
+mkdir -p ./distributions
+# rm -rf ./distributions/SpotiWeb-win32-ia32.zip
+rm -rf ./distributions/SpotiWeb-win32-x64.zip
 
 cd releases
-echo '#### Compressing win ia32 ####'
-zip -r -X ../dist/SpotiWeb-win32-ia32.zip ./SpotiWeb-win32-ia32
-# echo '#### Compressing win x64 ####'
-# zip -r -X ../dist/SpotiWeb-win32-x64.zip ./SpotiWeb-win32-x64
+# echo '#### Compressing win ia32 ####'
+# zip -r -X ../distributions/SpotiWeb-win32-ia32.zip ./SpotiWeb-win32-ia32
+echo '#### Compressing win x64 ####'
+zip -r -X ../distributions/SpotiWeb-win32-x64.zip ./SpotiWeb-win32-x64
 
 echo '#### Done Windows versions ####'
